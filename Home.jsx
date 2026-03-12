6aae694d3fa8783a82a7a4d3314501e7faa6fbfe
boopi26 <24bct110@kgcas.com>
2026-03-12 15:04:36 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="home">
+
+      <h1>Coupon & Offer Management System</h1>
+      <h2>WELCOME</h2>
+
+      
+
+    </div>
+  );
+}
+
+export default Home;
